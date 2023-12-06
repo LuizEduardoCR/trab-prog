5e6dbc0b69dedf84e3e62f57552ffbbc6dc608ff
LuizEduardoCR <luizeduardocabraldarosa@gmail.com>
2023-12-06 16:51:20 -0300

    comentarios adicionados e teclado quase todo certo

--- calc.js
+++ calc.js
@@ -1,77 +1,79 @@
-const previousOperationText = document.querySelector("#previous-operation");
-const currentOperationText = document.querySelector("#current-operation");
-const buttons = document.querySelectorAll("#buttons-container button");
+//seleciona os elementos para retornar valores 
 
-class Calculator {
-  constructor(previousOperationText, currentOperationText) {
-    this.previousOperationText = previousOperationText;
-    this.currentOperationText = currentOperationText;
-    this.currentOperation = "";
+const previousOperationText = document.querySelector("#previous-operation");//declara a constante e usa document.querySelector() para selecionar o previous-operation
+const currentOperationText = document.querySelector("#current-operation");//declara a constante e usa document.querySelector() para selecionar o current-operation
+const buttons = document.querySelectorAll("#buttons-container button");//declara a constante e usa document.querySelectorAll() para selecionar os botões dentro de buttons-container
+
+
+class Calculator { //Define a class Calculator
+  constructor(previousOperationText, currentOperationText) { //cria objetos
+    this.previousOperationText = previousOperationText;//propriedade do objeto
+    this.currentOperationText = currentOperationText;//propriedade do objeto
+    this.currentOperation = ""; //inicia a propriedade vazia, pois a operação atual vai aí
   }
 
-  // add digit to calculator screen
-  addDigit(digit) {
-    console.log(digit);
+  // coloca o numero na tela
+  addDigit(digit) {//representa o dígito a ser adicionado na operação atual.
     // Check if number already has a dot
     if (digit === "." && this.currentOperationText.innerText.includes(".")) {
-      return;
+      return; //ve se a operação tem ponto, e se tiver, retorna nada.
     }
 
     this.currentOperation = digit;
     this.updateScreen();
   }
 
-  // process all calculator operations
+  // processa as operações da calculadora
   processOperation(operation) {
-    // Check if current value is empty
+    // vê se o valor atual ta vazio
     if (this.currentOperationText.innerText === "" && operation !== "C") {
-      // Change operation
+      // muda a operação
       if (this.previousOperationText.innerText !== "") {
         this.changeOperation(operation);
       }
       return;
     }
 
-    // Get current and previous values
+    // pega o valor atual e anterior 
     let operationValue;
-    let previous = +this.previousOperationText.innerText.split(" ")[0];
-    let current = +this.currentOperationText.innerText;
+    let previous = +this.previousOperationText.innerText.split(" ")[0]; //define que a let é igual o conteudo do previousOperation
+    let current = +this.currentOperationText.innerText; //define que a let é igual o conteudo do currentOperation
 
     switch (operation) {
       case "+":
-        operationValue = previous + current;
+        operationValue = previous + current; //se for +, soma o valor anterior com o atual
         this.updateScreen(operationValue, operation, current, previous);
         break;
       case "-":
-        operationValue = previous - current;
+        operationValue = previous - current;//se for -, subtrai o valor anterior com o atual
         this.updateScreen(operationValue, operation, current, previous);
         break;
       case "X":
-        operationValue = previous * current;
+        operationValue = previous * current;//se for X, multiplica o valor anterior com o atual
         this.updateScreen(operationValue, operation, current, previous);
         break;
       case "÷":
-        operationValue = previous / current;
+        operationValue = previous / current;//se for ÷, divide o valor anterior com o atual
         this.updateScreen(operationValue, operation, current, previous);
         break;
       case "DEL":
-        this.processDelOperator();
+        this.processDelOperator();//chama a função para a ação
         break;
       case "CE":
-        this.processClearCurrentOperator();
+        this.processClearCurrentOperator();//chama a função para a ação
         break;
       case "C":
-        this.processClearOperator();
+        this.processClearOperator();//chama a função para a ação
         break;
       case "=":
-        this.processEqualOperator();
+        this.processEqualOperator();//chama a função para a ação
         break;
       default:
         return;
     }
   }
 
-  // Change values of calculator screen
+  // muda os valores na calculadora
   updateScreen(
     operationValue = null,
     operation = null,
@@ -79,52 +81,54 @@ class Calculator {
     previous = null
   ) {
     if (operationValue === null) {
-      // Append number to current value
+      // colocar numero no valor atual
       this.currentOperationText.innerText += this.currentOperation;
     } else {
-      // Check if value is zero, if is just add current value
+      // ver se é zero, se for coloca o valor atual
       if (previous === 0) {
         operationValue = current;
       }
-      // Add current value to previous
+      // bota o valor atual como anterior 
       this.previousOperationText.innerText = `${operationValue} ${operation}`;
       this.currentOperationText.innerText = "";
     }
   }
 
-  // Change math operation
+  // trocar as operações
   changeOperation(operation) {
-    const mathOperations = ["X", "-", "+", "÷"];
+    const mathOperations = ["X", "-", "+", "÷"];//deixa as operações matematicas como constantes
 
-    if (!mathOperations.includes(operation)) {
+    if (!mathOperations.includes(operation)) {//ve se o valor pra trocar a operação ta na const mathOperations, se n tiver, retorna um grande nada
       return;
     }
 
     this.previousOperationText.innerText =
-      this.previousOperationText.innerText.slice(0, -1) + operation;
+      this.previousOperationText.innerText.slice(0, -1) + operation;//tira o ultimo operador e concatena a proxima operação
   }
 
-  // Delete a digit
-  processDelOperator() {
+  // Deleta um digito
+    processDelOperator() {
     this.currentOperationText.innerText =
-      this.currentOperationText.innerText.slice(0, -1);
+      this.currentOperationText.innerText.slice(0, -1);//o slice (0, -1) tira fora o ultimo numero digitado
   }
 
-  // Clear current operation
+  // limpa o valor atual
   processClearCurrentOperator() {
-    this.currentOperationText.innerText = "";
+    this.currentOperationText.innerText = ""; //isso faz com que o valor atual fique vazio
   }
 
-  // Clear all operations
+  // limpa todas as operações
   processClearOperator() {
-    this.currentOperationText.innerText = "";
-    this.previousOperationText.innerText = "";
+    this.currentOperationText.innerText = "";//deixa o valor atual vazio
+    this.previousOperationText.innerText = "";//deixa o valor anterior vazio
   }
 
-  // Process an operation
+  // processa uma operação
   processEqualOperator() {
+    // obtém a operação realizada anteriormente
     let operation = this.previousOperationText.innerText.split(" ")[1];
 
+    //faz com que seja processada a operação de acordo com o operador identificado
     this.processOperation(operation);
   }
 }
@@ -132,14 +136,39 @@ class Calculator {
 const calc = new Calculator(previousOperationText, currentOperationText);
 
 buttons.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    const value = e.target.innerText;
+    btn.addEventListener("click", (e) => {
+      // pega o valor do texto do botão clicado
+      const value = e.target.innerText;
+  
+      // ve se o valor é um número positivo ou um ponto decimal 
+      if (+value >= 0 || value === ".") {
+        console.log(value);
+        // se for um número ou um ponto, adiciona o dígito à operação atual na calculadora
+        calc.addDigit(value);
+      } else {
+        // se não for um número ou ponto, processa a operação correspondente na calculadora
+        calc.processOperation(value);
+      }
+    });
+  });
 
-    if (+value >= 0 || value === ".") {
-      console.log(value);
-      calc.addDigit(value);
-    } else {
-      calc.processOperation(value);
+// Event listener para capturar eventos de teclado
+document.addEventListener('keydown', (event) => {
+    const keyValue = event.key; // pega o valor da tecla pressionada
+  
+    // ve se a tecla pressionada é um número, ponto ou operação matemática
+    if (!isNaN(keyValue) || keyValue === '.' || ['+', '-', '*', '/'].includes(keyValue)) {
+      // se for um número, ponto ou operação, adiciona o dígito à operação atual na calculadora
+      calc.addDigit(keyValue);
+    } else if (keyValue === 'Enter') {
+      // se a tecla pressionada for 'Enter', processa a operação de igual na calculadora
+      calc.processOperation('=');
+    } else if (keyValue === 'Backspace') {
+      // Se a tecla pressionada for 'Backspace', processa a operação de deleção na calculadora
+      calc.processOperation('DEL');
+    } else if (keyValue === 'Escape') {
+      // Se a tecla pressionada for 'Escape', processa a operação de limpar tudo na calculadora
+      calc.processOperation('C');
     }
   });
-});
+  
